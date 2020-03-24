c8354a73347e7c563d4b56bb30c4bfdbb9327699
Tyler Porter <tyler.b.porter@gmail.com>
2020-03-23 20:34:10 -0400

    Handle original documentation indentation level

--- lib/doc_writer.rb
+++ lib/doc_writer.rb
@@ -9,10 +9,11 @@ class DocWriter
 
     def write_docs(docs, fragment_title)
       old_documentation = File.read(default_docs_path)
-      new_documentation_fragment = example_hash(fragment_title, old_documentation)
-      return if new_documentation_fragment.nil?
+      old_documentation_fragment = example_hash(fragment_title, old_documentation)
+      return if old_documentation_fragment.nil?
 
-      new_documentation = old_documentation.sub(new_documentation_fragment, docs)
+      new_documentation_fragment = apply_original_indentation(docs, fragment_title)
+      new_documentation = old_documentation.sub(old_documentation_fragment, new_documentation_fragment)
       File.open(default_docs_path, 'w') { |f| f.write(new_documentation) }
     end
 
@@ -33,6 +34,16 @@ class DocWriter
 
         iterator += 1
       end
+    end
+
+    def apply_original_indentation(new_documentation, fragment_title)
+      indentation = ' ' * original_indent_level(fragment_title)
+      new_documentation.split("\n").join("\n" + indentation)
+    end
+
+    def original_indent_level(fragment_title)
+      file = File.read(default_docs_path)
+      file.match(/\s+(?=#{fragment_title})/)[0].size - 1
     end
   end
 end
